408f6d394165b9b4590d1273ee6836c6d4909c06
Samuel <smfikiru@gmail.com>
2026-02-21 00:18:01 +0300

    made the task checking work for the first click

--- script.js
+++ script.js
@@ -9,52 +9,86 @@ let todoLists = [
   "Clean the room",
 ];
 
-
-
 const addButton = document.querySelector(".add-task-btn");
-addButton.addEventListener('click', ()=>{
-    const todoName = document.querySelector(".js-task-input");
-    todoLists.push(todoName.value);
-    renderHtml();
-    todoName.value = '';
-})
+addButton.addEventListener("click", () => {
+  const todoName = document.querySelector(".js-task-input");
+  todoLists.push(todoName.value);
+  renderHtml();
+  todoName.value = "";
+});
 
-function handleDeleteBtn(){
-    const deleteButton = document.querySelectorAll(".js-delete-icon-container");
-    console.log(deleteButton);
-    deleteButton.forEach((deletebtn) => {
+function handleDeleteBtn() {
+  const deleteButton = document.querySelectorAll(".js-delete-icon-container");
+  deleteButton.forEach((deletebtn) => {
     deletebtn.addEventListener("click", () => {
-        console.log("clicked");
-        const todoIndex = deletebtn.dataset["itemIndex"];
-        todoLists.splice(todoIndex, 1);
-        renderHtml();
+      const todoIndex = deletebtn.dataset["itemIndex"];
+      todoLists.splice(todoIndex, 1);
+      renderHtml();
     });
-    });
+  });
 }
 
-
+let objectList;
+let todoHTMLfinal = "";
 renderHtml();
-function renderHtml(){
-    let todoHtml = "";
-    todoLists.forEach((todo, index)=>{
-        todoHtml += `
-        <div class="task-item">
-        <p class="task-name">
+
+function renderHtml() {
+  let todoHtml = [];
+  todoLists.forEach((todo, index) => {
+    todoHtml.push({
+      index: `<div class="task-item">
+        <p class="task-name js-todo-name">
             ${todo}
         </p>
         <div class="delete-icon-container js-delete-icon-container" data-item-index="${index}">
             <img class="delete-icon" src="images/delete-icon.png" alt="">
         </div>
         </div>
-        <hr>
-        `;
-    })
+        <hr>`,
+      name: `${todo}`,
+    });
+  });
 
-    document.querySelector(".js-task-list-container").innerHTML = todoHtml; 
-    handleDeleteBtn();
+  objectList = todoHtml;
+  displayHtml();
 }
 
+function displayHtml() {
+  objectList.forEach((code, index) => {
+    todoHTMLfinal += objectList[index].index;
+  });
+  document.querySelector(".js-task-list-container").innerHTML = todoHTMLfinal;
+  handleDeleteBtn();
+}
 
+let newList = [];
+const todoNameList = document.querySelectorAll(".js-todo-name");
 
+todoNameList.forEach((name) => {
+  name.addEventListener("click", () => {
+    newList = [];  
+    objectList.forEach((obj) => {
+      const tempdiv = document.createElement("div");
+      tempdiv.innerHTML = obj.index;
+      const updateElement = tempdiv.querySelector(".task-name");
 
+      if (
+        tempdiv.querySelector(".task-name").innerText.trim() === name.innerText
+      ) {
+        updateElement.classList.add("completed-todo");
+      }
 
+      const objformat = {
+        index: tempdiv.innerHTML,
+        name: name.innerText
+      };
+      newList.push(objformat);
+    });
+    objectList = newList;
+    todoHTMLfinal = '';
+    displayHtml();
+
+    console.log(newList);
+  });
+
+});
