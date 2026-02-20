f8d68dbcd2560f3b712b46a54795a7f9a4285d72
Samuel <smfikiru@gmail.com>
2026-02-20 21:31:17 +0300

    made the add button interactive

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-let todoLists = ["Prepare meal for me", 'Lunch with Zack'];
+let todoLists = [
+  "Buy groceries",
+  "Finish VS Code project",
+  "Call Alice",
+  "Read JavaScript tutorial",
+  "Exercise for 30 minutes",
+  "Plan weekend trip",
+  "Pay bills",
+  "Clean the room",
+];
 
 
-let todoHtml = '';
 
-todoLists.forEach((todoName)=>{
-    todoHtml += `
-    <div class="task-item">
-      <p class="task-name">
-        ${todoName}
-      </p>
-      <div class="delete-icon-container">
-        <img class="delete-icon" src="images/delete-icon.png" alt="">
-      </div>
-    </div>
-    <hr>
-    `;
+const addButton = document.querySelector(".add-task-btn");
+addButton.addEventListener('click', ()=>{
+    const todoName = document.querySelector(".js-task-input");
+    todoLists.push(todoName.value);
+    renderHtml();
+    todoName.value = '';
 })
 
-document.querySelector(".js-task-list-container").innerHTML =todoHtml;
+function handleDeleteBtn(){
+
+}
+
+
+renderHtml();
+
+function renderHtml(){
+
+    let todoHtml = "";
+    todoLists.forEach((todo, index)=>{
+        todoHtml += `
+        <div class="task-item" data-itemIndex="${index}">
+        <p class="task-name">
+            ${todo}
+        </p>
+        <div class="delete-icon-container .js-delete-icon-container">
+            <img class="delete-icon" src="images/delete-icon.png" alt="">
+        </div>
+        </div>
+        <hr>
+        `;
+    })
+
+    document.querySelector(".js-task-list-container").innerHTML = todoHtml; 
+    
+    handleDeleteBtn();
+}
+
+
+
+
+
+const deleteButton = document.querySelectorAll(".js-delete-icon-container");
+console.log(deleteButton);
