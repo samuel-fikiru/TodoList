927abd7b4a530555758da414ed4a22b4253c3742
samuel <smfikiru@gmail.com>
2026-02-26 14:19:43 +0300

    fully functional todo

--- script.js
+++ script.js
@@ -1,120 +1,80 @@
 let todoLists = [
-  "Buy groceries",
-  "Finish VS Code project",
-  "Call Alice",
-  "Read JavaScript tutorial",
-  "Exercise for 30 minutes",
-  "Plan weekend trip",
-  "Pay bills",
-  "Clean the room",
+  { name: "Buy groceries", completed: false },
+  { name: "Finish VS Code project", completed: false },
+  { name: "Call Alice", completed: false },
 ];
 
+// adding new task by add button and enter key
 const addButton = document.querySelector(".add-task-btn");
-
-addButton.addEventListener("click", () => {
-  const todoName = document.querySelector(".js-task-input");
-  todoLists.push(todoName.value);
-  renderHtml();
-  todoName.value = "";
-});
-
 const inputField = document.querySelector(".js-task-input");
 
-inputField.addEventListener("keypress", (event) => {
+addButton.addEventListener("click", () => {
+  const taskName = document.querySelector(".js-task-input");
+  todoLists.push({ name: taskName.value, completed: false });
+  renderHtml();
+  taskName.value = "";
+});
+
+// adding new task by enter key
+inputField.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    const todoName = document.querySelector(".js-task-input");
-    todoLists.push(todoName.value);
+    todoLists.push({ name: inputField.value, completed: false });
     renderHtml();
-    todoName.value = "";
+    inputField.value = "";
   }
 });
 
-
-function handleDeleteBtn() {
-  const deleteButton = document.querySelectorAll(".js-delete-icon-container");
-  deleteButton.forEach((deletebtn) => {
-    deletebtn.addEventListener("click", () => {
-      const todoIndex = deletebtn.dataset["itemIndex"];
-      
-      throughEventListener();
-      todoLists.splice(objectList[todoIndex], 1);
-      console.log(objectList);
-      renderHtml();
-    });
-  });
-}
-
-let objectList;
-let todoHTMLfinal = "";
+// Desplaying the task on the webpage
 renderHtml();
-
 function renderHtml() {
-  let todoHtml = [];
+  let todoHtml = "";
   todoLists.forEach((todo, index) => {
-    todoHtml.push({
-      index: `<div class="task-item">
-        <p class="task-name js-todo-name">
-            ${todo}
+    const completedTask = todo.completed ? "completed-todo" : "";
+    todoHtml += `<div class="task-item">
+        <p class="task-name js-todo-name ${completedTask}" data-task-index=${index}>
+            ${todo.name}
         </p>
         <div class="delete-icon-container js-delete-icon-container" data-item-index="${index}">
             <img class="delete-icon" src="images/delete-icon.png" alt="">
         </div>
         </div>
-        <hr>`,
-      name: `${todo}`,
+        <hr>`;
+  });
+  document.querySelector(".js-task-list-container").innerHTML = todoHtml;
+  handleDeleteBtn();
+  markComplited();
+}
+
+// deletes element when delete icon clicked
+function handleDeleteBtn() {
+  const deleteButtons = document.querySelectorAll(".js-delete-icon-container");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const todoIndex = button.dataset["itemIndex"];
+      todoLists.splice(Number(todoIndex), 1);
+      renderHtml();
     });
   });
-
-  objectList = todoHtml;
-  displayHtml();
+  starterText();
 }
 
-function displayHtml() {
-  todoHTMLfinal = "";
-  objectList.forEach((code, index) => {
-    todoHTMLfinal += objectList[index].index;
-  });
-  document.querySelector(".js-task-list-container").innerHTML = todoHTMLfinal;
-  handleDeleteBtn();
-  throughEventListener();
+// placeholder text for empty todo lists
+function starterText() {
+  noTaskElement = document.querySelector(".js-task-list-container");
+  if (noTaskElement.innerHTML === "") {
+    noTaskElement.innerHTML = `<p class="initial-text">Add a Todo List</p>`;
+  }
 }
 
+// mark/unmarks todos
+function markComplited() {
+  const Tasks = document.querySelectorAll(".js-todo-name");
+  Tasks.forEach((task) => {
+    task.addEventListener("click", () => {
+      const index = task.dataset.taskIndex;
 
-
-
-function throughEventListener() {
-  let newList = [];
-
-  const todoNameList = document.querySelectorAll(".js-todo-name");
- // console.log(todoNameList);
-  todoNameList.forEach((name) => {
-    name.addEventListener("click", () => {
-      const clickedTodoName = name.innerText;
-      newList = [];
-      objectList.forEach((obj) => {
-        const tempdiv = document.createElement("div");
-        tempdiv.innerHTML = obj.index;
-        const updateElement = tempdiv.querySelector(".task-name");
-
-        if (
-          tempdiv.querySelector(".task-name").innerText.trim() ===
-          name.innerText
-        ) {
-          updateElement.classList.add("completed-todo");
-        }
-
-        const objformat = {
-          index: tempdiv.innerHTML,
-          name: name.innerText,
-        };
-        newList.push(objformat);
-      });
-
-      //console.log(newList);
-
-      objectList = newList;
-      todoHTMLfinal = "";
-      displayHtml();
+      todoLists[index].completed = todoLists[index].completed ? false : true;
+      renderHtml();
     });
   });
 }
